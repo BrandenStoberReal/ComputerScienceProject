1d090f7056ab73b23027077b2a74aadf0782054d
Branden Stober <brandenstober@outlook.com>
2021-10-29 07:52:28 -0400

    Added math library

--- libs/Functions.js
+++ libs/Functions.js
@@ -1,7 +1,12 @@
 /*
     Branden's Functions Module
 */
-
+let Maths = {
+    RandomInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min) // Math is not mine, I am not a mathematician
+    }
+}
+  
 module.exports = {
-    
+    Maths
 }
